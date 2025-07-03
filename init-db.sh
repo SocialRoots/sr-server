a246eb1d4036993630bd617c30f788f5b72067fc
Nigini Oliveira <dev+github@nigini.me>
2025-07-03 16:37:25 -0700

    Fixed issues with db connection from user module.

--- init-db.sh
+++ init-db.sh
@@ -2,18 +2,20 @@
 source ./.env
 #printenv | grep POSTGRES
 
-DB_PREFIX="SR-"
 SR_SERVICES=("ORCHESTRATOR" "USERS" "GROUPS" "NOTES" "RESPONSES" "NOTIFICATIONS" "CONNECTIONS")
 #GET installed submodules modules to execute migrations
 SR_MIGRATION_PATH="./modules/SR_MODULE_FOLDER/scripts/migrate.sh"
+
+#TODO: How to not depend on this hardcoded module naming?
 SR_MODULE_DIR_PREFIX="RS-"
 SR_MODULES=$(ls -N ./modules/ | grep $SR_MODULE_DIR_PREFIX)
 
 export PGPASSWORD="$POSTGRES_PASSWORD"
 for service in "${SR_SERVICES[@]}"
 do
-  DB_NAME=$DB_PREFIX"$service"
-  psql -U "$POSTGRES_USER" -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -w "socialroots-default" \
+  DB_ENV_NAME="DB_NAME_$service"
+  DB_NAME=${!DB_ENV_NAME}
+  psql -U "$POSTGRES_USER" -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -w "$POSTGRES_DB" \
       -c "CREATE DATABASE \"$DB_NAME\";" \
       -c "GRANT ALL PRIVILEGES ON DATABASE \"$DB_NAME\" to \"$POSTGRES_USER\";"
   echo "$DB_NAME... done!"
@@ -28,7 +30,8 @@ export POSTGRES_USER=$POSTGRES_USER
 export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
 for module in $SR_MODULES
 do
-   export POSTGRES_DB_NAME=$DB_PREFIX${module/$SR_MODULE_DIR_PREFIX/}
-   echo "=> MIGRATING $module INTO $POSTGRES_DB_NAME"
-   eval "${SR_MIGRATION_PATH/SR_MODULE_FOLDER/$module} migrate"
+  DB_ENV_NAME="DB_NAME_${module/$SR_MODULE_DIR_PREFIX/}"
+  export POSTGRES_DB_NAME=${!DB_ENV_NAME}
+  echo "=> MIGRATING $module INTO $POSTGRES_DB_NAME"
+  eval "${SR_MIGRATION_PATH/SR_MODULE_FOLDER/$module} migrate"
 done
