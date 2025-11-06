a406073acf776cbc541ae38a3cf898329f87f2cf
Nigini Oliveira <dev+github@nigini.me>
2025-11-05 21:22:27 -0800

    Added Nostr Gateway

--- init-db.sh
+++ init-db.sh
@@ -2,13 +2,14 @@
 source ./.env
 #printenv | grep POSTGRES
 
-SR_SERVICES=("ORCHESTRATOR" "USERS" "GROUPS" "NOTES" "RESPONSES" "NOTIFICATIONS" "CONNECTIONS" "MEMBRANE")
+SR_SERVICES=("ORCHESTRATOR" "USERS" "GROUPS" "NOTES" "RESPONSES" "NOTIFICATIONS" "CONNECTIONS" "MEMBRANE" "NOSTR_GATEWAY")
 #GET installed submodules modules to execute migrations
 SR_MIGRATION_PATH="./modules/SR_MODULE_FOLDER/scripts/migrate.sh"
 
 #TODO: How to not depend on this hardcoded module naming?
 SR_MODULE_DIR_PREFIX="RS-"
-SR_MODULES=$(ls ./modules/ | grep $SR_MODULE_DIR_PREFIX)
+SR_MODULE_DIR_PREFIX_ALT="SR-"
+SR_MODULES=$(ls ./modules/ | grep -E "^($SR_MODULE_DIR_PREFIX|$SR_MODULE_DIR_PREFIX_ALT)")
 
 export PGPASSWORD="$POSTGRES_PASSWORD"
 for service in "${SR_SERVICES[@]}"
@@ -30,7 +31,12 @@ export POSTGRES_USER=$POSTGRES_USER
 export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
 for module in $SR_MODULES
 do
-  DB_ENV_NAME="DB_NAME_${module/$SR_MODULE_DIR_PREFIX/}"
+  # Remove either RS- or SR- prefix to get DB_NAME
+  DB_SUFFIX="${module/$SR_MODULE_DIR_PREFIX/}"
+  DB_SUFFIX="${DB_SUFFIX/$SR_MODULE_DIR_PREFIX_ALT/}"
+  # Replace hyphens with underscores for env var name
+  DB_SUFFIX="${DB_SUFFIX//-/_}"
+  DB_ENV_NAME="DB_NAME_${DB_SUFFIX}"
   export POSTGRES_DB_NAME=${!DB_ENV_NAME}
   echo "=> MIGRATING $module INTO $POSTGRES_DB_NAME"
   eval "${SR_MIGRATION_PATH/SR_MODULE_FOLDER/$module} migrate"
