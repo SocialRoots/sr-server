e16a271fe23d0067a1154511c0cd34e7cd1835da
Nigini Oliveira <dev+github@nigini.me>
2026-03-05 11:32:19 -0800

    Fixed some metadata and configuration after initial refacoring of modules.

--- bin/init-db.sh
+++ bin/init-db.sh
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-source ./.env
+#source ./.env
 #printenv | grep POSTGRES
 
 SR_SERVICES=("ORCHESTRATOR" "USERS" "GROUPS" "NOTES" "RESPONSES" "NOTIFICATIONS" "CONNECTIONS")
